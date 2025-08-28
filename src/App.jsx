b3132e26cb166b6c7988c2d377468c391597eee9
sudhanshu kumar <sudhanshu_24a12res696@iitp.ac.in>
2025-08-28 15:52:52 +0530

    added REACT JS file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Component/Navbar";
+import Hero from "./Component/Hero";
+function App() {
+  return (
+    <div>
+    <Hero/>
+    < Navbar />
+    
+    </div>
+  );
+}
+export default App;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Navbar () {
+    return <h1> hello </h1>
+    
+}
+export default Navbar;
